4941fdd2feea8da5be3feead70bebec29511f0e9
Youssef Maged <joaomaged@gmail.com>
2025-09-03 11:27:34 +0300

    adding stripe

--- YoussefWeb/Program.cs
+++ YoussefWeb/Program.cs
@@ -5,6 +5,8 @@ using Youssef.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
 using Youssef.Utility;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Youssef.Models;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe")); 
 
 
 builder.Services.AddIdentity<IdentityUser,IdentityRole>
@@ -42,7 +46,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:secretKey").Get<string>();
 app.UseRouting();
 
 app.UseAuthentication(); /* this means that it will check if the username and password exixsts in database or not (it must come before Authorization)*/
