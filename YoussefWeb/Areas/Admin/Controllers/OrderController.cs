04e301b5660ca218b162c629402190de5506f401
Youssef Maged <joaomaged@gmail.com>
2025-09-06 11:37:02 +0300

    OrderHeader in Admin

---
+++ YoussefWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Youssef.DataAccess.Repository.IRepository;
+using Youssef.Models;
+
+namespace YoussefWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitofwork;
+        private readonly IWebHostEnvironment _webhostenvironment;
+        public OrderController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitofwork = unitOfWork;
+            _webhostenvironment = webHostEnvironment;
+        }
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+        #region API CALLS
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderheaders = _unitofwork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderheaders });
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var productToBeDeleted = _unitofwork.Product.Get(u => u.ProductID == id);
+            if (productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            if (!string.IsNullOrEmpty(productToBeDeleted.ImageURL))
+            {
+
+                var OldImagePath
+                = Path.Combine(_webhostenvironment.WebRootPath, productToBeDeleted.ImageURL.TrimStart('\\'));
+
+                if (System.IO.File.Exists(OldImagePath))
+                {
+                    System.IO.File.Delete(OldImagePath);
+                }
+
+            }
+
+            _unitofwork.Product.Remove(productToBeDeleted);
+            _unitofwork.save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+        #endregion
+    }
+}
---
+++ YoussefWeb/wwwroot/js/Order.js
@@ -0,0 +1,39 @@
+﻿var dataTable;
+
+
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/Admin/Order/GetAll' },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "15%" },
+            { data: 'phonenumber', "width": "20%" },
+            { data: 'applicationuser.email', "width": "15%" },
+            { data: 'orderstatus', "width": "10%" },
+            { data: 'status', "width": "10%" },
+            { data: 'status', "width": "10%" },
+            {
+                data: 'productID',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group rounded" role="group">
+                    <a href="/Admin/order/details?orderid=${data}" class="btn btn-primary rounded mx-2"> <i class="bi bi-pencil-square"></i>  </a>
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
+
+
+
+
+
+
+
